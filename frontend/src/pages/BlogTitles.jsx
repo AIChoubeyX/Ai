acc52fc1c8b47df6ffe825788fe44f45a2c179ed
Chooubeyji <ashutoshchoubey2004@gmail.com>
2025-07-16 00:17:39 +0530

    BlogTitle part completed

--- frontend/src/pages/BlogTitles.jsx
+++ frontend/src/pages/BlogTitles.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
+import { useState } from 'react';
+import { Hash, Sparkles } from 'lucide-react';
 
 const BlogTitles = () => {
 
-  const articleLength = [
-      { length: 800, text: "short(500-800 words)" },
-      { length: 1200, text: "medium(800-1200 words)" },
-      { length: 1600, text: "long(1200+ words)" },
+  const blogCategories = [
+    'General' ,'Technology' , 'Business', 'Health', 'Lifestyle' , 'Eduction' ,'Travel', 'Food'    
     ];
   
-    const [selectedLength, setSelectedLength] = useState(articleLength[0]);
+    const [selectedCategory, setSelectedCategory] = useState('General');
     const [input, setInput] = useState("");
   
     const onSubmitHandler = async(e) => {
@@ -21,10 +21,10 @@ const BlogTitles = () => {
       onSubmit={onSubmitHandler}
       className="w-full   max-w-lg p-4 bg-white rounded-lg border border-gray-200">
         <div className="flex items-center gap-3">
-          <Sparkles className="w-6 text-[#4A74FF]" />
-          <h1 className="text-xl font-semibold">Article Configuration</h1>
+          <Sparkles className="w-6 text-[#8E37EB]" />
+          <h1 className="text-xl font-semibold">AI Title Generator</h1>
         </div>
-        <p className="mt-6 text-sm font-medium">Article Topic</p>
+        <p className="mt-6 text-sm font-medium">Keyword</p>
         <input
           onChange={(e) => setInput(e.target.value)}
           value={input}
@@ -33,40 +33,40 @@ const BlogTitles = () => {
           placeholder="The Future of Artificial intelligence is ...."
           reqired
         />
-        <p className="mt-4 text-sm font-medium">Article Length</p>
+        <p className="mt-4 text-sm font-medium">Category</p>
         <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
-          {articleLength.map((item, index) => (
+          {blogCategories.map((item) => (
             <span
-              onClick={() => setSelectedLength(item)}
+              onClick={() => setSelectedCategory(item)}
               className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
-                selectedLength.text === item.text
-                  ? "bg-blue-50 text-blue-700"
+                selectedCategory === item
+                  ? "bg-purple-50 text-purple-700"
                   : "text-gray-500 border-gray-300"
               }`}
-              key={index}
+              key={item}
             >
-              {item.text}
+              {item}
             </span>
           ))}
         </div>
         <br/>
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white py-2 px-4 mt-6
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F3] to-[#8E37EB] text-white py-2 px-4 mt-6
         text-sm rounded-lg cursor-pointer">
-          <Edit className="w-6" />
-          Generate Article
+          <Hash className="w-6" />
+          Generate title
         </button>
       </form>
       {/* right col */}
 
-      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
           <div className="flex items-center gap-3">
-            <Edit className="w-5 h-5 text-[#4A74FF]" />
-            <h1 className="text-xl font-semibold">Generated article</h1>
+            <Hash className="w-5 h-5 text-[#8E37EB]" />
+            <h1 className="text-xl font-semibold">Generated titles</h1>
           </div>
           <div className="flx-1 flex justify-center items-center">
             <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-              <Edit className="w-9 h-9" />
-              <p>Enter a topic and click 'Generate article' to get started</p>
+              <Hash className="w-9 h-9" />
+              <p>Enter a topic and click 'Generate title' to get started</p>
 
             </div>
 
